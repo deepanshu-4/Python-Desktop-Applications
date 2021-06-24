a47726111e3aeb78b48807ae17ce95062fb3756c
deepanshu-4 <65032759+deepanshu-4@users.noreply.github.com>
2021-06-25 00:09:34 +0530

    Add files via upload

---
+++ facedetector.bat
@@ -0,0 +1,4 @@
+@echo off
+cd C:\Users\chetna kaushik\Desktop
+python facedetector.py
+cd C:\Users\chetna kaushik\Desktop 
---
+++ facedetector.py
@@ -0,0 +1,98 @@
+import cv2
+import time
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders 
+
+cam = cv2.VideoCapture(0)
+time.sleep(4)
+cv2.namedWindow("test")
+
+img_counter = 0
+print("start")
+while img_counter!=1:
+    
+    ret, frame = cam.read()
+    if not ret:
+        print("failed to grab frame")
+        break
+    cv2.imshow("test", frame)
+
+    k = cv2.waitKey(1)
+    if k%256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+    # SPACE pressed
+    
+    time.sleep(1)
+    img_name = "opencv_frame_{}.png".format(img_counter)
+    cv2.imwrite(img_name, frame)
+    cam.release()
+    cv2.destroyAllWindows()
+    print("{} written!".format(img_name))
+    img_counter += 1
+
+    fromaddr = "kaushikdeepanshu14@gmail.com"
+    toaddr = "kaushikdeepanshu14@gmail.com"
+
+    # instance of MIMEMultipart 
+    msg = MIMEMultipart() 
+
+    # storing the senders email address 
+    msg['From'] = fromaddr 
+
+    # storing the receivers email address 
+    msg['To'] = toaddr 
+
+    # storing the subject 
+    msg['Subject'] = "Subject of the Mail"
+
+    # string to store the body of the mail 
+    body = "your pc has been start"
+
+    # attach the body with the msg instance 
+    msg.attach(MIMEText(body, 'plain')) 
+
+    # open the file to be sent 
+    filename = img_name
+    attachment = open(r"C:\Users\chetna kaushik\Desktop\opencv_frame_0.png", "rb") 
+
+    # instance of MIMEBase and named as p 
+    p = MIMEBase('application', 'octet-stream') 
+
+    # To change the payload into encoded form 
+    p.set_payload((attachment).read()) 
+
+    # encode into base64 
+    encoders.encode_base64(p) 
+
+    p.add_header('Content-Disposition', "attachment; filename= %s" % filename) 
+
+    # attach the instance 'p' to instance 'msg' 
+    msg.attach(p) 
+
+    # creates SMTP session 
+    s = smtplib.SMTP('smtp.gmail.com', 587) 
+
+    # start TLS for security 
+    s.starttls() 
+
+    # Authentication 
+    s.login(fromaddr, "anjubala") 
+
+    # Converts the Multipart msg into a string 
+    text = msg.as_string() 
+
+    # sending the mail 
+    s.sendmail(fromaddr, toaddr, text) 
+
+    # terminating the session 
+    s.quit() 
+
+
+
+
+cv2.destroyAllWindows()
